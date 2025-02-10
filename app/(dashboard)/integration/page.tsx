7159d196d43e0bd0c6d400c6dc40ec1dd54a7a6a
Mauro Cesar Restrepo Marichal <mauro.restrepo@globant.com>
2025-02-10 02:48:14 -0300

    minor

--- app/(dashboard)/integration/page.tsx
+++ app/(dashboard)/integration/page.tsx
@@ -8,11 +8,13 @@ import LinkIcon from "@mui/icons-material/Link";
 export default async function IntegrationPage() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Typography marginBlock={2}>Ingrese los datos del vehiculo</Typography>
+      <Typography marginBlock={2}>
+        Seleccione el proveedor con el que desea integrarse
+      </Typography>
 
       <Box sx={{ flexGrow: 1, marginBlock: 3 }}>
         <Link
-          href="https://auth.mercadolibre.com.uy/authorization?response_type=code&client_id=3844293861730465&redirect_uri=https://8911-190-133-94-152.ngrok-free.app/callback"
+          href="https://auth.mercadolibre.com.uy/authorization?response_type=code&client_id=3844293861730465&redirect_uri=https://next-js-concept.vercel.app/callback"
           target="_blank"
           rel="noopener noreferrer"
         >
